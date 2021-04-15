24a33602b91fea54b7a6643c36ab488694fd4d31
John El Basha <johnelbasha@gmail.com>
2021-04-15 05:34:13 +0100

    added sign-up mechanism to fix bug after devise installation

--- spec/features/user_creates_a_new_question_spec.rb
+++ spec/features/user_creates_a_new_question_spec.rb
@@ -1,9 +1,18 @@
 require "rails_helper"
 
+def user_Sign_Up
+  click_link "Sign up"
+  fill_in "Email", with: "user@example.com"
+  fill_in "Password", with: "123456789"
+  fill_in "Password confirmation", with: "123456789"
+  click_button "Sign up"
+end
+
 RSpec.feature "creating a new question" do
   scenario "user provides all necessary inputs" do
     visit "/"
     click_link "New Question"
+    user_Sign_Up
     expect(page).to have_content("Post a new question")
     fill_in "Title", with: 'Why is the sky blue?'
     fill_in "Body", with: 'I really want to know and there are so many different theories'
@@ -15,6 +24,7 @@ RSpec.feature "creating a new question" do
   scenario "user forgets question title" do
     visit "/"
     click_link "New Question"
+    user_Sign_Up
     expect(page).to have_content("Post a new question")
     fill_in "Title", with: ''
     fill_in "Body", with: 'I have intentionally forgotten the title of the question'
@@ -26,6 +36,7 @@ RSpec.feature "creating a new question" do
   scenario "user forgets question body" do
     visit "/"
     click_link "New Question"
+    user_Sign_Up
     expect(page).to have_content("Post a new question")
     fill_in "Title", with: 'Bodyless question'
     fill_in "Body", with: ''
@@ -37,6 +48,7 @@ RSpec.feature "creating a new question" do
   scenario "Title is less than 4 characters" do
     visit "/"
     click_link "New Question"
+    user_Sign_Up
     expect(page).to have_content("Post a new question")
     fill_in "Title", with: '123'
     fill_in "Body", with: 'Hey. My title only has three characters. Is this allowed?'
@@ -48,6 +60,7 @@ RSpec.feature "creating a new question" do
   scenario "Body is less than 10 characters" do
     visit "/"
     click_link "New Question"
+    user_Sign_Up
     expect(page).to have_content("Post a new question")
     fill_in "Title", with: 'Reasonable Title for A Question'
     fill_in "Body", with: '123456789'
