39a251867446ea5914f66230390f55e4526b90d6
John El Basha <johnelbasha@gmail.com>
2021-04-15 15:39:16 +0100

    updated specs for username attribute

--- spec/features/user_sees_all_questions_spec.rb
+++ spec/features/user_sees_all_questions_spec.rb
@@ -1,14 +1,26 @@
 require "rails_helper"
 
+def user_Sign_Up
+  click_link "Sign up"
+  fill_in "Email", with: "user@example.com"
+  fill_in "Username", with: "UsherPenguin"
+  fill_in "user[password]", with: "123456789"
+  fill_in "user[password_confirmation]", with: "123456789"
+  click_button "Sign up"
+end
+
 RSpec.feature "Viewing all questions" do
-  before do
-    @user = User.create(email: "test@gmail.com", password: "987654321", username: "BoJo")
-    @question_one = Question.create(title: "this is the first q", body: "this is the message body", user: @user)
-  end
 
   scenario 'A user navigates to homepage to see all the existing questions' do
     visit "/"
-      expect(page).to have_link(@question_one.title)
-      expect(page).to have_content(@question_one.user.username)
+    click_link "New Question"
+    user_Sign_Up
+    expect(page).to have_content("Post a new question")
+    fill_in "Title", with: 'Why is the sky blue?'
+    fill_in "Body", with: 'I really want to know and there are so many different theories'
+    click_button "Create Question"
+    expect(page).to have_link "Why is the sky blue?"
+    puts page.html
+    expect(page).to have_content("UsherPenguin")
   end
 end
